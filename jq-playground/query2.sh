5e17f9ed2cc13082a5b32480d1ac28394156e9c0
jramsey1186 <jramsey1186@gmail.com>
2024-08-17 14:23:02 +0000

    update to read JSON from tmp file

--- jq-playground/query2.sh
+++ jq-playground/query2.sh
@@ -1,4 +1,6 @@
-NAME="exp-2"
-
-ID=$(name=$NAME jq '.experiments[] | select(.tags.name == env.name) | .id' < example.json)
+NAME="exp-1"
+INPUT=.input
+cat example.json > .input
+ID=$(name=$NAME jq '.experiments[] | select(.tags.name == env.name) | .id' < $INPUT)
 echo $ID
+rm .input
